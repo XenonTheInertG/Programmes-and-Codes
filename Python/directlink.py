50115c2107051c1c2f29a5575212bbc3c9e1cc75
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-09-21 17:28:28 +0600

    Initial commit

---
+++ Python/directlink.py
@@ -0,0 +1,75 @@
+#Direct link generator
+#Module by ©XenonTheInertG
+
+import re
+
+from bs4 import BeautifulSoup
+
+from hitsuki.decorator import register
+
+from .utils.disable import disableable_dec
+from .utils.http import http
+from .utils.language import get_strings_dec
+from .utils.message import get_arg, get_cmd
+
+
+@register(cmds="direct")
+@disableable_dec("direct")
+@get_strings_dec("direct_links")
+async def direct_link_generator(message, strings):
+    text = get_arg(message)
+
+    if not text:
+        await message.reply(strings["cmd_example"].format(cmd=get_cmd(message)))
+        return
+
+    links = re.findall(r"\bhttps?://.*\.\S+", text)
+
+    reply = []
+    if not links:
+        await message.reply(strings["no_link"])
+        return
+
+    for link in links:
+        if "sourceforge.net" in link:
+            reply.append(await sourceforge(link, strings))
+        else:
+            reply.append(
+                re.findall(r"\bhttps?://(.*?[^/]+)", link)[0] + " is not supported"
+            )
+
+    await message.reply("\n".join(reply))
+
+
+async def sourceforge(url: str, strings) -> str:
+    try:
+        link = re.findall(r"\bhttps?://.*sourceforge\.net\S+", url)[0]
+    except IndexError:
+        reply = strings["no_sf_link"]
+        return reply
+
+    file_path = re.findall(r"/files(.*)/download", link)
+    if not file_path:
+        file_path = re.findall(r"/files(.*)", link)
+    try:
+        file_path = file_path[0]
+    except IndexError:
+        reply = strings["sf_link_error"]
+        return reply
+    reply = f"Mirrors for <code>{file_path.split('/')[-1]}</code>\n"
+    project = re.findall(r"projects?/(.*?)/files", link)[0]
+    mirrors = (
+        f"https://sourceforge.net/settings/mirror_choices?"
+        f"projectname={project}&filename={file_path}"
+    )
+    response = await http.get(mirrors)
+    page = BeautifulSoup(response.content, "lxml")
+    info = page.find("ul", {"id": "mirrorList"}).findAll("li")
+
+    for mirror in info[1:]:
+        name = re.findall(r"\((.*)\)", mirror.text.strip())[0]
+        dl_url = (
+            f'https://{mirror["id"]}.dl.sourceforge.net/project/{project}/{file_path}'
+        )
+        reply += f'<a href="{dl_url}">{name}</a> '
+    return reply
